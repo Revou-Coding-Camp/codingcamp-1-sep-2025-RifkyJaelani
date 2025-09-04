3fa37466606bc0da21850c9137aaae1f6c77580f
Muhamad Rifky Jaelani <163749238+RifkyJaelani@users.noreply.github.com>
2025-09-05 00:40:49 +0700

    add script.js & style.css file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/// variabel to store todo items
+let todoList = [];
+
 function validateInput() {
   const todoInput = document.getElementById("todo-input").value.trim();
   const todoDateInput = document.getElementById("todo-date-input").value;
@@ -10,4 +13,14 @@ function validateInput() {
   // kalau lolos validasi, panggil addTodo()
   addTodo(todoInput, todoDateInput);
   return true;
+}
+
+function addTodo(todo, dueDate) {
+  const todoItem = {
+    task: todo,
+    dueDate: dueDate,
+    completed: false,
+  };
+  todoList.push(todoItem);
+  renderTodoList();
 }
